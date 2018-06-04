3e5140a8fb2ff8feded3aef14332bc7021972d6d
Thevaa Chandereng <chandereng@wisc.edu>
2018-06-04 15:38:30 -0500

    enrollment dates function update

--- R/enrollment.R
+++ R/enrollment.R
@@ -3,30 +3,45 @@
 #' This function simulates enrollment dates using either poisson or binomial distribution
 #'
 #' @param family a factor value of either "poisson" or "binomial"
-#' @param param a numeric value of lambda in poisson or value of a in uniform(0, a)
+#' @param param a vector of lambda in poisson
+#' @param time a vector of the same size as param indicating end of time when a specific lambda is used
 #' @param N_total a numeric value of
 #' @return a vector of enrollment dates
 #'
-#' @examples enrollment("uniform", 3, 100)
+#' @examples enrollment(param = c(0.003, 0.7), 100, time = c(10, 400))
 #'
 #'
 #'
 
 
-enrollment <- function(family = "poisson", param, N_total){
-  stopifnot((family == "uniform" | family == "poisson"), is.numeric(param), param > 0)
+enrollment <- function(param, N_total, time = NULL){
+  #checking each inputs
+  stopifnot(all(param > 0), if(length(param) >= 1){!is.null(time)})
   output <- NULL
-  if(family == "uniform"){
-    output <- round(runif(N_total, 0, param) + cumsum(c(0, rep(param, (N_total - 1)))))
-  }
-  else if(family == "poisson"){
-    count <- 0
+  count <- 0
+  # for constant lambda in poisson
+  if(length(param) == 1){
     while(length(output) < N_total){
-      output <- c(output, rep(count, rpois(1, param)))
       count <- count + 1
+      output <- c(output, rep(count, rpois(1, param)))
     }
-    output <- output[1:N_total]
-    output <- output - output[1]
   }
+  # for different lambda in poisson a function of time
+  else{
+    while(length(output) < N_total){
+      count <- count + 1
+      #if the time exceeds count, reuse the last lambda
+      if(sum(time >= (count - 1)) == 0){
+        index <- length(param)
+      }
+      #else pick the minimum time where time exceeds count
+      else{
+        index <- min(which(time >= (count - 1)))
+      }
+      output <- c(output, rep(count, rpois(1, param[index])))
+    }
+  }
+  output <- output[1:N_total]
+  output <- output - output[1]
   output
 }
