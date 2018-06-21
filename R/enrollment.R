a7d8eb050409dec428f93144601cef8c5ef48e3f
Thevaa Chandereng <chandereng@wisc.edu>
2018-06-21 16:37:31 -0500

    fixed bug in enrollment

--- R/enrollment.R
+++ R/enrollment.R
@@ -9,7 +9,7 @@
 #'
 #' @examples
 #' enrollment(param = c(0.003, 0.7), 100, time = 10)
-#' enrollment(param = c(0.3, 0.5, 0.9, 1.2, 2.1), 200, c(20, 30, 40, 60, 200))
+#' enrollment(param = c(0.3, 0.5, 0.9, 1.2, 2.1), 200, c(20, 30, 40, 60))
 #'
 #'
 
