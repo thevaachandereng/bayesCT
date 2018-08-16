f13dcc2fddb0653cef8ec1b5bad8ef384b688a80
Thevaa Chandereng <chandereng@wisc.edu>
2018-08-16 14:47:42 -0500

    added more test for randomization

--- tests/testthat/test_randomization.R
+++ tests/testthat/test_randomization.R
@@ -4,6 +4,8 @@ test_that("The randomization is", {
   check <- sample(rep(0:1, c(60, 40)))
   set.seed(200)
   expect_equal(randomization(100, 100, c(3, 2)), check)
+  set.seed(40555)
+  expect_equal((randomization(12, 10, c(3, 2)) == 1), 5)
   expect_error(randomization(20, 40, c(1, 4)))
   expect_error(randomization(100, 20, c(2, 1)))
 })
