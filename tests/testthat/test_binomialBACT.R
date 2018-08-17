809a20374f1a03a4d8e9c0a6b25eda2cb7fb996a
Thevaa Chandereng <chandereng@wisc.edu>
2018-08-17 09:38:10 -0500

    add binomialBACT test

---
+++ tests/testthat/test_binomialBACT.R
@@ -0,0 +1,19 @@
+input <- list(p_control = 0.12, p_treatment = 0.10,
+              y0_treatment = 8, N0_treatment = 90, N_impute = 10,
+              y0_control = 13, N0_control = 95, N_total = 300,
+              lambda = c(0.3, 1), lambda_time = c(25),
+              interim_look = c(220, 270), EndofStudy = 50)
+
+
+context("")
+test_that("The binomial bayesCT is ", {
+  set.seed(200)
+  expect_equal(do.call(binomialBACT, input)$p_treatment, 0.10)
+  expect_error(do.call(binomialBACT, input))
+})
+
+
+
+
+
+
