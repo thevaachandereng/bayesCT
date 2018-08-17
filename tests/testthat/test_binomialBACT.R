905588ffee8a9437f3f0032d219e5b72b1eab6d2
Thevaa Chandereng <chandereng@wisc.edu>
2018-08-17 12:02:52 -0500

    added more test

--- tests/testthat/test_binomialBACT.R
+++ tests/testthat/test_binomialBACT.R
@@ -4,11 +4,16 @@ input1 <- list(p_control = 0.12, p_treatment = 0.08,
                lambda = c(0.3, 1), lambda_time = c(25),
                interim_look = c(220, 270), EndofStudy = 50)
 
+
 input2 <- list(p_treatment = 0.09, EndofStudy = 50,
                y0_treatment = 8, N0_treatment = 90,
                N_impute = 10, N_total = 300,
                lambda = c(0.3, 1), lambda_time = c(25),
                interim_look = c(220, 270))
+
+input3 <- list(p_control = 0.12, p_treatment = 0.08, N_impute = 10,
+               N_total = 500, lambda = c(0.3, 1), lambda_time = c(25),
+               interim_look = c(420, 470), EndofStudy = 50)
 
 context("")
 test_that("The binomial bayesCT is ", {
@@ -17,6 +22,8 @@ test_that("The binomial bayesCT is ", {
   set.seed(12225)
   expect_equal(BACTbinomial(input1)$p_control, 0.12)
   expect_equal(do.call(binomialBACT, input2)$p_treatment, 0.09)
+  set.seed(20288)
+  expec_equal(BACTbinomial(input3)$prob_of_accepting_alternative, 0.95)
   input1$p_control <- 1.2
   expect_error(do.call(binomialBACT, input1))
 })
