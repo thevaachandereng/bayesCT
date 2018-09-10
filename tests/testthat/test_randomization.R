fa22f6f84325c8b5bcfa59350242d8fd7778b7a1
Thevaa Chandereng <chandereng@wisc.edu>
2018-09-10 15:22:31 -0500

    Revert "fixed testthat case"
    This reverts commit 8a70b47973135b9009b5a48357c61ebf25885c61.

--- tests/testthat/test_randomization.R
+++ tests/testthat/test_randomization.R
@@ -5,7 +5,7 @@ test_that("The randomization is", {
   set.seed(200)
   expect_equal(randomization(100, 100, c(3, 2)), check)
   set.seed(40555)
-  expect_equal(sum(randomization(12, 10, c(3, 2)) == 1), 4)
+  expect_equal(sum(randomization(12, 10, c(3, 2)) == 1), 5)
   expect_error(randomization(20, 40, c(1, 4)))
   expect_error(randomization(100, 20, c(2, 1)))
 })
