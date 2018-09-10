301b73313eb65cb51c8ddf761fc688ee80a87a69
Thevaa Chandereng <chandereng@wisc.edu>
2018-09-10 14:59:34 -0500

    added an example

--- R/randomization.R
+++ R/randomization.R
@@ -9,11 +9,15 @@
 #' @return the randomization allocation with 0, 1 for control and treatment
 #'
 #' @examples
-#' # Implementing randomization allocation for control to treatment with 1:1.5 randomization ratio
+#' # Implementing treatment allocation for control to treatment with 1:1.5 randomization ratio
 #' randomization(N_total = 100, block = 5, allocation = c(2, 3))
 #'
-#' # Randomization allocation with 2:1 for control to treatment
+#' # Treatment allocation with 2:1 for control to treatment
 #' randomization(N_total = 70, block = 9, allocation = c(2, 1))
+#'
+#' # Treatment allocation for control to treatment with 1:2 for control to treatment with
+#' # multiple block sizes c(3, 9, 6)
+#' randomization(N_total = 100, block = c(3, 9, 6), allocation = c(1, 2))
 #'
 #' # For complete randomization set the N_total to block size
 #' randomization(N_total = 100, block = 100, allocation = c(1, 1))
