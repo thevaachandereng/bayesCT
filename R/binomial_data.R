5d48a14b9a20fdc816998257ca96103de216e6f7
Thevaa Chandereng <chandereng@wisc.edu>
2018-11-26 13:01:37 -0600

    updated dataset

---
+++ R/binomial_data.R
@@ -0,0 +1,19 @@
+#' Binomial dataset for analyzing adaptive Bayesian trials
+#'
+#' A dataset containing the results of 300 patients with binomial outcome,
+#' the dataset is filled with loss to follow up.
+#'
+#' @format A data frame with 300 rows and 4 variables:
+#' \describe{
+#'   \item{ID}{Patient ID in the trial}
+#'   \item{treatment}{treatment assignment for patients, 1 for treatment group
+#'   0 for control group}
+#'   \item{results}{binomial outcome of the trial, 1
+#'   for response (success or failure), 0 for no response}
+#'   \item{complete}{1 for complete outcome, 0 for loss to follow up}
+#' }
+#' @export
+#' @examples
+#' data(binomial_data)
+
+"binomial_data"
