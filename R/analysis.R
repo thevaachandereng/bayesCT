35dfc529c2569173edc54d74edde3fe6f637c194
Thevaa Chandereng <chandereng@wisc.edu>
2019-01-07 07:56:36 -0600

    added analysis wrapper

---
+++ R/analysis.R
@@ -0,0 +1,28 @@
+#' @title Analysis wrapper function
+#'
+#' @description Wrapper function to analyze bayesian trials.
+#'
+#' @param input list. Input function for all the analysis.
+#' @param type character. Type of analysis to be ran (binomial (default),
+#'   normal. etc.).
+#' @param .data NULL. stores the all the details, please do not fill it in.
+#'
+#' @return a list with results of the analysis of bayesian trial.
+#'
+#' @importFrom stats rbinom glm rnorm lm
+#' @importFrom dplyr mutate filter group_by bind_rows select n
+#' @importFrom bayesDP bdpbinomial bdpnormal
+#'
+#' @export analysis
+#'
+
+analysis <- function(input, type = "binomial", .data = NULL){
+  if(type == "binomial"){
+    do.call(binomial_analysis, input)
+  }
+
+  if(type == "normal"){
+    do.call(normal_analysis, input)
+  }
+
+}
