4b5de7308246e3542aaf8fa75b9587f592008cab
Thevaa Chandereng <chandereng@wisc.edu>
2019-01-18 02:38:30 -0600

    added return value for simulate

--- R/simulate.R
+++ R/simulate.R
@@ -6,7 +6,34 @@
 #' @param no_of_sim numeric. Number of simulations to run
 #' @param .data NULL. stores the proportion of control and treatment, please do not fill it in.
 #'
-#' @return a list with results of the simulation (power and type I error).
+#' @return a list with results of the simulation (power and type I error) and the input.
+#'
+#' \describe{
+#'   \item{\code{input}}{
+#'   A list of input values used in the trial simulation.}
+#'   \item{\code{power}}{
+#'     data_frame. A data frame with the interim look and power at each look.}
+#'   \item{\code{type1_error}}{
+#'     scalar. The type 1 error or the number of times the trial rejects the null
+#'     when the parameters are simulated under the null hypothesis.}
+#'   \item{\code{est_final}}{
+#'     vector. The final estimate of the difference in posterior estimate of
+#'     treatment and posterior estimate of the control group for all the
+#'     simulation.}
+#'   \item{\code{post_prob_accept_alternative}}{
+#'     vector. The final probability of accepting the alternative for the
+#'     simulations.}
+#'   \item{\code{N_enrolled}}{
+#'     vector. The number of patients enrolled in the trial (sum of control
+#'     and experimental group for each simulation.)}
+#'   \item{\code{stop_futility}}{
+#'     vector. Did the trial stop for futility during imputation of patient
+#'     who had loss to follow up? 1 for yes and 0 for no.}
+#'   \item{\code{stop_expected_success}}{
+#'     vector. Did the trial stop for early success during imputation of patient
+#'     who had loss to follow up? 1 for yes and 0 for no.}
+#'
+#' }
 #'
 #' @importFrom stats rbinom glm
 #' @importFrom dplyr mutate filter group_by bind_rows select n
@@ -87,13 +114,13 @@ simulate <- function(input, no_of_sim = 10000, .data = NULL){
 
   type1_error <- mean(prob_ha_t1 > output_power[[1]]$prob_of_accepting_alternative)
 
-  return(list(power                         = power,
+  return(list(input                         = input,
+              power                         = power,
               type1_error                   = type1_error,
               est_final                     = est_final,
               post_prob_accept_alternative  = prob_ha,
               N_enrolled                    = N_stop,
               stop_expect_success           = expect_success,
               stop_futility                 = stop_fail))
-
 
 }
