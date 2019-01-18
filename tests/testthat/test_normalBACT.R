eb1940f0b7d471d51487b14071c9bf5fee6b3b68
Thevaa Chandereng <chandereng@wisc.edu>
2019-01-18 03:18:01 -0600

    added test case and fixed man file

--- tests/testthat/test_normalBACT.R
+++ tests/testthat/test_normalBACT.R
@@ -5,3 +5,18 @@ input1 <- list(mu_control = 10, mu_treatment = 8,
                interim_look = c(220, 270),
                N_impute = 20)
 
+input2 <- list(mu_treatment = 8, sd_treatment = 1,
+               N_total = 300, EndofStudy = 50,
+               lambda = c(0.3, 1), lambda_time = c(25),
+               interim_look = c(220, 270), h0 = 2,
+               N_impute = 20)
+
+context("")
+test_that("The binomial bayesCT is ", {
+  set.seed(200)
+  expect_equal(do.call(normalBACT, input1)$mu_treatment, 8)
+  expect_equal(do.call(normalBACT, input2)$mu_treatment, 8)
+  input1$sd_control <- -1.2
+  expect_error(do.call(normalBACT, input1))
+
+})
