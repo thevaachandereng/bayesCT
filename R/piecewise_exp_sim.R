77d96336e86f8bf8fcb76485b1468b5443a9df1e
Thevaa Chandereng <chandereng@wisc.edu>
2019-11-17 04:15:55 -0600

    fixed issue with pw_exp

--- R/piecewise_exp_sim.R
+++ R/piecewise_exp_sim.R
@@ -4,7 +4,7 @@
 #' the piecewise constant hazard exponential function.
 #'
 #' @param lambda vector. The constant hazard rates for exponential failures.
-#' @param tau vector. The change-point vector indicating time when the hazard rates change.
+#' @param cutpoint vector. The change-point vector indicating time when the hazard rates change.
 #' @param n scalar. The number of outcomes for simulation.
 #' @param maxtime scalar. maximum time before end of study.
 #'
@@ -19,7 +19,7 @@
 #' @export pw_exp_sim
 
 
-pw_exp_sim <- function(lambda, n, maxtime, tau = NULL) {
+pw_exp_sim <- function(lambda, n, maxtime, cutpoint = NULL) {
   ## checking input of parameters
   #make sure lambda is positive
   if(any(lambda < 0)) {stop("At least one of the hazard rate(s) is less than 0!")}
@@ -33,43 +33,44 @@ pw_exp_sim <- function(lambda, n, maxtime, tau = NULL) {
 
   #make sure lambda is positive
   if(length(lambda) > 1) {
-    if(length(lambda) != (length(tau) + 1)){
-      stop("The length of tau needs to be one less than the length of lambda!")
+    if(length(lambda) != (length(cutpoint) + 1)){
+      stop("The length of cutpoint needs to be one less than the length of lambda!")
     }
   }
 
-  # make sure tau is null if length of lambda is 1.
+  # make sure cutpoint is null if length of lambda is 1.
   if(length(lambda) == 1){
-    if(!is.null(tau)){
-      stop("Tau needs to be null if length of lambda is 1!")
+    if(!is.null(cutpoint)){
+      stop("cutpoint needs to be null if length of lambda is 1!")
     }
   }
 
   # generate exp(1)
   x <- rexp(n)
 
-  if(is.null(tau)) {
+  if(is.null(cutpoint)) {
     time <- x / lambda
   }
 
-  if(length(tau) >= 2) {
-    lamtau <- c(lambda[1] * tau[1], length(lambda))
-    if(length(tau) > 1){
-      for(h in 2:length(tau)){
-        lamtau[h] <- lambda[h] * (tau[h] - tau[h - 1])
+  if(length(cutpoint) >= 1) {
+    lamtau <- lambda[1] * cutpoint[1]
+    if(length(cutpoint) > 1){
+      lamtau <- c(lamtau, rep(NA, (length(lambda) - 1)))
+      for(h in 2:length(cutpoint)){
+        lamtau[h] <- lambda[h] * (cutpoint[h] - cutpoint[h - 1])
       }
     }
     lamtau  <- cumsum(lamtau)
     lamtau[length(lamtau) + 1] <- 99999
     cp <- function(p){
       time  <- rep(NA, length(p))
-      for(n in 1:length(p)){
-        index <- min(which(p[n] < lamtau))
-      if(p[n] < lamtau[1]){
-        time[n] <-  p[n] / lambda[1]
+      for(m in 1:length(p)){
+        index <- min(which(p[m] < lamtau))
+      if(p[m] < lamtau[1]){
+        time[m] <-  p[m] / lambda[1]
       }
-      else if(p[n] >= lamtau[1]){
-        time[n] <- ((p[n] - lamtau[(index - 1)]) / lambda[index] + tau[index - 1])
+      else if(p[m] >= lamtau[1]){
+        time[m] <- ((p[m] - lamtau[(index - 1)]) / lambda[index] + cutpoint[index - 1])
         }
       }
       return(time)
@@ -85,4 +86,5 @@ pw_exp_sim <- function(lambda, n, maxtime, tau = NULL) {
   # return dataset with time and event
   return(dat)
 }
+
 
