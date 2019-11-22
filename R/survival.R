cb56c40e31c5bff1b4ff357f14304f7f6814da2e
Thevaa Chandereng <chandereng@wisc.edu>
2019-11-22 15:47:43 -0600

    fixed r code with survival

--- R/survival.R
+++ R/survival.R
@@ -13,13 +13,19 @@
 #'   all subjects are assumed to have an event.
 #' @param time0 vector. Historical exposure time for the subjects. It must be the same length
 #'    as the treatment variable.
-#' @param event vector. Hisc status indicator, normally 0=alive, 1=dead.
+#' @param event0 vector. Historical status indicator, normally 0=alive, 1=dead.
 #'   Other choices are TRUE/FALSE (TRUE = death) or 1/2 (2=death).
 #'   For censored data, the status indicator is 0=right censored, 1 = event at time.
 #'   Although unusual, the event indicator can be omitted, in which case
 #'   all subjects are assumed to have an event.
 #' @param prior scalar. Prior value for the gamma shape of the piecewise
 #'   exponential hazards. Default is c(0.1, 0.1).
+#' @surv_time scalar. scalar. Survival time of interest for computing the probability
+#'    of survival for a single arm (OPC) trial. Default is overall, i.e.,
+#'    current+historical, median survival time.
+#' @breaks vector. Breaks (interval starts) used to compose the breaks of the piecewise
+#'     exponential model. Do not include zero. Default breaks are the quantiles of the input
+#'     times.
 #' @inheritParams binomial_analysis
 #'
 #' @importFrom survival Surv
@@ -69,18 +75,19 @@ survival_analysis <- function(
   treatment,
   time,
   event                 = NULL,
-  treatment_0           = NULL,
-  time_0                = NULL,
-  event_0               = NULL,
+  treatment0            = NULL,
+  time0                 = NULL,
+  event0                = NULL,
   surv_time             = NULL,
+  h0                    = 0,
+  breaks                = NULL,
   alternative           = "greater",
   N_impute              = 10,
-  h0                    = 0,
   number_mcmc           = 10000,
   prob_ha               = 0.95,
   futility_prob         = 0.10,
   expected_success_prob = 0.90,
-  prior                 = c(1, 1),
+  prior                 = c(.1, .1),
   discount_function     = "identity",
   fix_alpha             = FALSE,
   alpha_max             = 1,
@@ -95,7 +102,10 @@ survival_analysis <- function(
 
   # combining the histrotical data
   if(!is.null(time0)){
-    data0 <- data.frame(cbind(time0, event0, treatment0))
+    data0 <- data.frame(cbind(time = time0, event = event0, treatment = treatment0))
+  }
+  else{
+    data0 <- NULL
   }
 
   #reading the data
@@ -103,11 +113,13 @@ survival_analysis <- function(
 
 
   # analyze the data using bayesDp
-  post <- bdpsurvival(formula    = Surv(time, status) ~ treatment,
-                      data       = data_total,
-                      data0      = data0,
-                      fix_alpha  = TRUE,
-                      method     = "fixed")
+  post <- bdpsurvival(formula      = Surv(time, event) ~ treatment,
+                      data         = data_total,
+                      data0        = data0,
+                      fix_alpha    = TRUE,
+                      number_mcmc  = number_mcmc,
+                      breaks       = breaks,
+                      method       = "fixed")
 
 
   # assigning stop_futility and expected success
@@ -115,140 +127,120 @@ survival_analysis <- function(
   stop_expected_success <- 0
   expected_success_test <- 0
 
-  for(i in 1:N_impute){
-    data_control_success_impute <- data_interim %>%
-      filter(treatment == 0) %>%
-      mutate(outcome_impute = ifelse(futility,
-                                     rbinom(n(), 1, prop$p_outcome[1]),
-                                     outcome))
-    # imputing success for treatment group
-    data_treatment_success_impute  <- data_interim %>%
-      filter(treatment == 1) %>%
-      mutate(outcome_impute = ifelse(futility,
-                                     rbinom(n(), 1, prop$p_outcome[2]),
-                                     outcome))
+  # for(i in 1:N_impute){
+  #   data_control_success_impute <- data_interim %>%
+  #     filter(treatment == 0) %>%
+  #     mutate(outcome_impute = ifelse(futility,
+  #                                    rbinom(n(), 1, prop$p_outcome[1]),
+  #                                    outcome))
+  #   # imputing success for treatment group
+  #   data_treatment_success_impute  <- data_interim %>%
+  #     filter(treatment == 1) %>%
+  #     mutate(outcome_impute = ifelse(futility,
+  #                                    rbinom(n(), 1, prop$p_outcome[2]),
+  #                                    outcome))
+  #
+  #   # combine the treatment and control imputed datasets
+  #   data_success_impute <- bind_rows(data_control_success_impute,
+  #                                    data_treatment_success_impute) %>%
+  #     mutate(outcome = outcome_impute) %>%
+  #     select(-outcome_impute)
+  #
+  #   # Create enrolled subject data frame for discount function analysis
+  #   data <- data_success_impute
+  #
+  #
+  #
+  #   # analyze complete+imputed data using discount funtion via binomial
+  #   post_imp <- bdpbinomial(y_t                    = sum(data$outcome[data$treatment == 1]),
+  #                           N_t                    = length(data$outcome[data$treatment == 1]),
+  #                           y_c                    = y_c,
+  #                           N_c                    = N_c,
+  #                           y0_t                   = y0_treatment,
+  #                           N0_t                   = N0_treatment,
+  #                           y0_c                   = y0_control,
+  #                           N0_c                   = N0_control,
+  #                           discount_function      = discount_function,
+  #                           number_mcmc            = number_mcmc,
+  #                           a0                     = prior[1],
+  #                           b0                     = prior[2],
+  #                           alpha_max              = alpha_max,
+  #                           fix_alpha              = fix_alpha,
+  #                           weibull_scale          = weibull_scale,
+  #                           weibull_shape          = weibull_shape)
+  #
+  #   if(sum(data$treatment == 0) != 0){
+  #     if(alternative == "two-sided"){
+  #       effect_imp <- post_imp$posterior_treatment$posterior - post_imp$posterior_control$posterior
+  #       success <- max(c(mean(effect_imp > h0), mean(-effect_imp > h0)))
+  #     }
+  #     else if(alternative == "greater"){
+  #       effect_imp <- post_imp$posterior_treatment$posterior - post_imp$posterior_control$posterior
+  #       success <- mean(effect_imp > h0)
+  #     }
+  #     else{
+  #       effect_imp <- post_imp$posterior_treatment$posterior - post_imp$posterior_control$posterior
+  #       success <- mean(-effect_imp > h0)
+  #     }
+  #   }
+  #
+  #   else{
+  #     effect_imp <- post_imp$final$posterior
+  #     if(alternative == "two-sided"){
+  #       success <- max(c(mean(effect_imp > h0), mean(effect_imp < h0)))
+  #     }
+  #     else if(alternative == "greater"){
+  #       success <- mean(effect_imp > h0)
+  #     }
+  #     else{
+  #       success <- mean(effect_imp < h0)
+  #     }
+  #   }
+  #
+  #   if(success > prob_ha){
+  #     expected_success_test <- expected_success_test + 1
+  #   }
+  #
+  # }
+  #
+  # if(expected_success_test / N_impute < futility_prob){
+  #   stop_futility       <- 1
+  # }
+  #
+  # # Test if expected success criteria met
+  # if(expected_success_test / N_impute > expected_success_prob ){
+  #   stop_expected_success <- 1
+  # }
 
-    # combine the treatment and control imputed datasets
-    data_success_impute <- bind_rows(data_control_success_impute,
-                                     data_treatment_success_impute) %>%
-      mutate(outcome = outcome_impute) %>%
-      select(-outcome_impute)
+  data_final <- data_total
 
-    # Create enrolled subject data frame for discount function analysis
-    data <- data_success_impute
-
-
-
-    # analyze complete+imputed data using discount funtion via binomial
-    post_imp <- bdpbinomial(y_t                    = sum(data$outcome[data$treatment == 1]),
-                            N_t                    = length(data$outcome[data$treatment == 1]),
-                            y_c                    = y_c,
-                            N_c                    = N_c,
-                            y0_t                   = y0_treatment,
-                            N0_t                   = N0_treatment,
-                            y0_c                   = y0_control,
-                            N0_c                   = N0_control,
-                            discount_function      = discount_function,
-                            number_mcmc            = number_mcmc,
-                            a0                     = prior[1],
-                            b0                     = prior[2],
-                            alpha_max              = alpha_max,
-                            fix_alpha              = fix_alpha,
-                            weibull_scale          = weibull_scale,
-                            weibull_shape          = weibull_shape)
-
-    if(sum(data$treatment == 0) != 0){
-      if(alternative == "two-sided"){
-        effect_imp <- post_imp$posterior_treatment$posterior - post_imp$posterior_control$posterior
-        success <- max(c(mean(effect_imp > h0), mean(-effect_imp > h0)))
-      }
-      else if(alternative == "greater"){
-        effect_imp <- post_imp$posterior_treatment$posterior - post_imp$posterior_control$posterior
-        success <- mean(effect_imp > h0)
-      }
-      else{
-        effect_imp <- post_imp$posterior_treatment$posterior - post_imp$posterior_control$posterior
-        success <- mean(-effect_imp > h0)
-      }
-    }
-
-    else{
-      effect_imp <- post_imp$final$posterior
-      if(alternative == "two-sided"){
-        success <- max(c(mean(effect_imp > h0), mean(effect_imp < h0)))
-      }
-      else if(alternative == "greater"){
-        success <- mean(effect_imp > h0)
-      }
-      else{
-        success <- mean(effect_imp < h0)
-      }
-    }
-
-    if(success > prob_ha){
-      expected_success_test <- expected_success_test + 1
-    }
-
-  }
-
-  if(expected_success_test / N_impute < futility_prob){
-    stop_futility       <- 1
-  }
-
-  # Test if expected success criteria met
-  if(expected_success_test / N_impute > expected_success_prob ){
-    stop_expected_success <- 1
-  }
-
-  data_final <- data_interim %>%
-    filter(!futility)
-
-  if(sum(data_final$treatment == 0) != 0){
-    y_c <- sum(data_final$outcome[data_final$treatment == 0])
-    N_c <- length(data_final$outcome[data_final$treatment == 0])
-  }
-  else{
-    y_c <- NULL
-    N_c <- NULL
-  }
 
   # Analyze complete data using discount funtion via binomial
-  post_final <- bdpbinomial(y_t                  = sum(data_final$outcome[data_final$treatment == 1]),
-                            N_t                  = length(data_final$outcome[data_final$treatment == 1]),
-                            y_c                  = y_c,
-                            N_c                  = N_c,
-                            y0_t                 = y0_treatment,
-                            N0_t                 = N0_treatment,
-                            y0_c                 = y0_control,
-                            N0_c                 = N0_control,
-                            number_mcmc          = number_mcmc,
-                            discount_function    = discount_function,
-                            a0                   = prior[1],
-                            b0                   = prior[2],
-                            alpha_max            = alpha_max,
-                            fix_alpha            = fix_alpha,
-                            weibull_scale        = weibull_scale,
-                            weibull_shape        = weibull_shape)
+  post_final <- bdpsurvival(formula      = Surv(time, event) ~ treatment,
+                            data         = data_final,
+                            data0        = data0,
+                            fix_alpha    = TRUE,
+                            number_mcmc  = number_mcmc,
+                            breaks       = breaks,
+                            method       = "fixed")
 
   ### Format and output results
   # Posterior effect size: test vs control or treatment itself
   if(sum(data_final$treatment == 0) != 0){
+    effect <- fit2$final$posterior_loghazard
     if(alternative == "two-sided"){
-      effect <- post_final$posterior_treatment$posterior - post_final$posterior_control$posterior
       post_paa <- max(c(mean(effect > h0), mean(-effect > h0)))
     }
     else if(alternative == "greater"){
-      effect <- post_final$posterior_treatment$posterior - post_final$posterior_control$posterior
       post_paa <- mean(effect > h0)
     }
     else{
-      effect <- post_final$posterior_treatment$posterior - post_final$posterior_control$posterior
       post_paa <- mean(-effect > h0)
     }
   }
 
   else{
-    effect <- post_final$final$posterior
+    effect <- post_final$posterior_treatment$posterior_survival
     if(alternative == "two-sided"){
       post_paa <- max(c(mean(effect > h0), mean(effect < h0)))
     }
@@ -260,14 +252,13 @@ survival_analysis <- function(
     }
   }
 
-  N_treatment  <- sum(data_final$treatment)         # Total sample size analyzed - test group
-  N_control    <- sum(!data_final$treatment)        # Total sample size analyzed - control group
-  N_enrolled   <- dim(data_total)[1]
+  N_treatment      <- sum(data_final$treatment)         # Total sample size analyzed - test group
+  N_control        <- sum(!data_final$treatment)        # Total sample size analyzed - control group
+  N_enrolled       <- N_treatment + N_control
 
-  #estimating prop
-  prop <- data %>%
-    group_by(treatment) %>%
-    summarize(p_outcome = mean(outcome))
+
+  event_treatment <- sum(data_final$event[data_final$treatment == 1])
+  event_control   <- sum(data_final$event[data_final$treatment == 0])
 
   ## output
   results_list <- list(
@@ -275,9 +266,10 @@ survival_analysis <- function(
     margin                                     = h0,                       # margin for error
     alternative                                = alternative,              # alternative hypothesis
     N_treatment                                = N_treatment,
+    event_treatment                            = event_treatment,
     N_control                                  = N_control,
+    event_control                              = event_control,
     N_complete                                 = N_treatment + N_control,
-    N_enrolled                                 = N_enrolled,               # Total sample size enrolled when trial stopped
     post_prob_accept_alternative               = post_paa,                 # Posterior probability that alternative hypothesis is true
     est_final                                  = mean(effect),             # Posterior Mean of treatment effect
     stop_futility                              = stop_futility,            # Did the trial stop for futility
