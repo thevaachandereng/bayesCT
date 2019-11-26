91109e4330a65aa75092b5c5004bb25f4be79899
Thevaa Chandereng <chandereng@wisc.edu>
2019-11-26 16:54:13 -0600

    added survival data

---
+++ R/survivaldata.R
@@ -0,0 +1,24 @@
+#' Survival dataset for analyzing adaptive Bayesian trials.
+#'
+#' A dataset containing the results of 100 patients with survival outcomes,
+#' the dataset is filled with treatment assignment, time and censoring.
+#'
+#' @docType data
+#'
+#' @keywords dataset
+#'
+#' @usage data(survivaldata)
+#'
+#' @format A data frame with 100 rows and 4 variables:
+#' \describe{
+#'   \item{id}{Patient ID in the trial}
+#'   \item{treatment}{treatment assignment for patients, 1 for treatment group
+#'   0 for control group}
+#'   \item{time}{the follow up time for patients}
+#'   \item{event}{The status indicator, normally 0=alive, 1=dead or
+#'   0 = no event, 1 = event occurred}
+#' }
+#'
+#' @examples
+#' data(survivaldata)
+NULL
