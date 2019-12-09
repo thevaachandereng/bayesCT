6e0c36200fc5fba8978143d691128acb224d9418
Thevaa Chandereng <chandereng@wisc.edu>
2019-12-09 11:52:01 -0600

    updatd all historical data test cases

--- tests/testthat/test_historical_binomial.R
+++ tests/testthat/test_historical_binomial.R
@@ -1,7 +1,10 @@
 context("")
 test_that("The historical_binomial are", {
   expect_equal(historical_binomial(y0_treatment = 10, y0_control = 12,
-                                   N0_treatment = 29, N0_control = 25)$N0_control, 25)
+                                   N0_treatment = 29, N0_control = 25,
+                                   discount_function = "weibull", alpha_max = 1,
+                                   fix_alpha = "FALSE", weibull_scale = 0.135, weibull_shape = 3,
+                                   method = "mc")$N0_control, 25)
   expect_equal(historical_binomial(y0_treatment = 10, y0_control = 12,
                                    N0_treatment = 29, N0_control = 25)$discount_function, "identity")
 })
--- tests/testthat/test_historical_normal.R
+++ tests/testthat/test_historical_normal.R
@@ -1,7 +1,10 @@
 context("")
 test_that("The historical_normal are", {
   expect_equal(historical_normal(mu0_treatment = 15, sd0_treatment = 2, N0_treatment = 10,
-                                 mu0_control = 17, sd0_control = 3, N0_control = 20)$sd0_control, 3)
+                                 mu0_control = 17, sd0_control = 3, N0_control = 20,
+                                 discount_function = "weibull", alpha_max = 1,
+                                 fix_alpha = "FALSE", weibull_scale = 0.135, weibull_shape = 3,
+                                 method = "mc")$sd0_control, 3)
   expect_equal(historical_normal(mu0_treatment = 15, sd0_treatment = 2, N0_treatment = 10,
                                  mu0_control = 17, sd0_control = 3, N0_control = 20)$discount_function, "identity")
 })
