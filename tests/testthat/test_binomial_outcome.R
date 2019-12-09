1bdb5271849ea2e28418c4469b28e4f560aef2e4
Thevaa Chandereng <chandereng@wisc.edu>
2019-12-09 11:04:03 -0600

    updated input

--- tests/testthat/test_binomial_outcome.R
+++
@@ -1,5 +0,0 @@
-context("")
-test_that("The binomial_outcome are", {
-  expect_equal(binomial_outcome(p_control = 0.91, p_treatment = 0.83)$p_control, 0.91)
-  expect_equal(binomial_outcome(p_control = 0.35, p_treatment = 0.32)$p_treatment, 0.32)
-})
