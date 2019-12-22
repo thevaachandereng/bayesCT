14ef10fe683f1f443da15f1d0390d90eda91822d
Thevaa Chandereng <chandereng@wisc.edu>
2019-12-21 23:58:48 -0600

    Revert "Revert "Revert "Update pw_exp.R"""
    This reverts commit b5093b1921f0a00b7cab7d32c88d8f9d7dbeee0e.

--- R/pw_exp.R
+++ R/pw_exp.R
@@ -29,15 +29,10 @@ pw_exp_sim <- function(hazard, n, maxtime = NULL, cutpoint = NULL) {
 
   #make sure maxtime is positive or null
   if(!is.null(maxtime)){
-    if(any(maxtime <= 0)) {
+    if(maxtime <= 0 | length(maxtime) > 1) {
       stop("The maxtime needs to be greater than 0 and
            the length of maxtime needs to be 1!")
       }
-  }
-
-  if(!is.null(maxtime) & length(maxtime) > 2){
-    maxtime <- maxtime[1]
-    warning("The first maxtime input is used!")
   }
 
   #make sure hazard is positive
